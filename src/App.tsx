2f78611524892b2a7b5f70eb8ca19291a86e9569
Portela03 <joao.portela03@gmai.com>
2024-01-06 21:38:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import Layout from './components/layout'
+import GlobalStyles from './styles/GlobalStyles'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Layout/>
+    <GlobalStyles/>
+    </>
+  )
+}
+
+export default App
